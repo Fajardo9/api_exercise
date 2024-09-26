1fcda7afb9f1de416f9cd7763d422fecea4972c6
Alejandro Fajardo <jorge.fajardo@globant.com>
2024-09-26 16:50:21 -0500

    Add documentation to the TestRunner to run all the test from this class

--- src/test/java/com/globant/api_tae/runner/TestRunner.java
+++ src/test/java/com/globant/api_tae/runner/TestRunner.java
@@ -4,9 +4,21 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-import java.lang.invoke.ConstantCallSite;
-
 import static com.globant.api_tae.utils.Constants.*;
+
+/**
+ * TestRunner class to execute all the test selected by it&acute;s tag.
+ *
+ * <p> This class runs the test based on the tags provided,ensuring that specific sets of test are executed as needed.
+ * The current tags available are:</p>
+ * <ul>
+ *     <li>{@code @smoke} -  executes all the available test to validate the basic functionalities</li>
+ *     <li>{@code @clientChangingNumber} - executes the test related to the client number changes</li>
+ *     <li>{@code @clientManagement} -  executes the tests related to the management of clients</li>
+ *     <li>{@code @resourceUpdate} -  execute the test related to the resource changes</li>
+ *      <li>{@code @resourceManagement} -  executes the tests related to the management of resources</li>
+ * </ul>
+ */
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
